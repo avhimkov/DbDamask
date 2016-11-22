934b09ed61e0929b54d36e8b33022b4a83ed7677
avhimkov <avhimkov@mail.ru>
2016-11-22 21:28:34 +0500

    add delete rowe

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,17 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         try {
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-            Connection conn = DriverManager.getConnection("jdbc:ucanaccess://D:\\1\\DamaskLN\\archive.accdb");
+            Connection conn = DriverManager.getConnection("jdbc:ucanaccess://D:\\1\\DamaskLN\\archive.mdb");
 
             Statement st = conn.createStatement();
 
-            String sql = "Select * from Process";
+            String sql = "DELETE FROM Process WHERE Ticket='20160413-000077'";
+            st.execute(sql);
 
-            ResultSet rs = st.executeQuery(sql);
-            while (rs.next()) {
-
-                System.out.println("\n" + rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4));
-            }
+//            ResultSet rs = st.executeQuery(sql);
+//            while (rs.next()) {
+//                System.out.println("\n" + rs.getString(1));
+//            }
 
         } catch (Exception e) {
 
