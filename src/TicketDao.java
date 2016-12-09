a17a1e09cfc6220b6a887e50308763cca20e22af
Dmitry <avhimkov@mail.ru>
2016-12-09 11:43:33 +0500

    add class TicketDao

--- src/TicketDao.java
+++ src/TicketDao.java
@@ -1,5 +1,10 @@
-/**
- * Created by adl on 09.12.16.
- */
-public class TicketDao {
+import com.agileasoft.jackcess.orm.dao.AccessDao;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TicketDao extends AccessDao<Ticket> {
+    public TicketDao(File accessFile) throws IOException {
+        super(accessFile, Ticket.class);
+    }
 }
