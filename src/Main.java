1644e947789c05154f8faf978e1fa759c805a445
avhimkov <avhimkov@mail.ru>
2016-12-11 22:31:00 +0500

    add work for quotes

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,12 @@
-import com.agileasoft.jackcess.orm.dao.AccessDao;
-
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
+import java.sql.*;
 import java.util.Scanner;
 
 
 public class Main {
-//    static Statement st = null;
-//    static ResultSet rs = null;
-//    static PreparedStatement pst = null;
-//    static Connection connection= null;
+
     public static void main(String[] args) throws SQLException {
 
-        AccessDao<Ticket> clientDao = new new TicketDao(new File("/path/to/your/mdb/file.mdb"));
-        List<Ticket> listClients = clientDao.findAll();
-        for(Ticket client : listClients) {
-            System.out.println(client.toString());
-        }
         try {
             /*чтение файла конфигурации*/
             InputStream myFile = new BufferedInputStream(new FileInputStream("config.txt"));
@@ -31,33 +17,16 @@ public class Main {
 
             BufferedReader inputTicket = new BufferedReader(new InputStreamReader(System.in));
             String ticket = inputTicket.readLine();
+            String text = "'"+ticket+"'";
 
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             Connection conn = DriverManager.getConnection(db);
             Statement st = conn.createStatement();
-            st.execute("DELETE FROM Process WHERE Ticket='20160413-0001010'");
-            st.execute("DELETE FROM Terminal WHERE Ticket='20160413-0001010'");
+            st.execute("DELETE FROM Process WHERE Ticket="+text);
+            st.execute("DELETE FROM Terminal WHERE Ticket="+text);
 
-//            String sql = "DELETE FROM Process WHERE Ticket=?"; //20160413-000044
-//            String sql = "DELETE FROM Terminal WHERE Ticket=?"; //20160413-000044
-//            pst = connection.prepareStatement(sql);
-//            pst.setString(1, "'20160413-000066'");
-//            pst.execute();
 
             System.out.println("Delete");
-
-//            ResultSet rs = st.executeQuery(sql);
-//            while (rs.next()) {
-//                System.out.println("\n" + rs.getString(1));
-//            }
-
-//            String dbFileSpec = "C:/Users/Public/example.accdb";
-//            try (Database db = DatabaseBuilder.open(new File(dbFileSpec))) {
-//                Table tbl = db.getTable("XXXX_XA_Data_1");
-//                HashMap rowData = new HashMap();
-//                rowData.put("Today's Future Variation Margin", 1);
-//                tbl.addRowFromMap(rowData);
-//            }
 
         } catch (Exception e) {
 
