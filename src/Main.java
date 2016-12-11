6e36b62d7dbf0db2b392f3812fa5bcb853ebf226
avhimkov <avhimkov@mail.ru>
2016-12-12 00:19:28 +0500

    add Set Tickets

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 
 public class Main {
@@ -15,9 +22,13 @@ public class Main {
 
             String db = "jdbc:ucanaccess://"+line+"archive.mdb"; //D:\1\DamaskLN\
 
-            BufferedReader inputTicket = new BufferedReader(new InputStreamReader(System.in));
-            String ticket = inputTicket.readLine();
-            String text = "'"+ticket+"'";
+//            BufferedReader inputTicket = new BufferedReader(new InputStreamReader(System.in));
+//            String ticket = inputTicket.readLine();
+            List<String> list = new ArrayList<>();
+            Files.lines(Paths.get("ticket.txt"), StandardCharsets.UTF_8).forEach(list::add);
+            //подстановка кавычек
+            String text = "'"+list+"'";
+            System.out.println(list);
 
             Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
             Connection conn = DriverManager.getConnection(db);
@@ -25,13 +36,10 @@ public class Main {
             st.execute("DELETE FROM Process WHERE Ticket="+text);
             st.execute("DELETE FROM Terminal WHERE Ticket="+text);
 
-
             System.out.println("Delete");
 
         } catch (Exception e) {
-
             System.out.println(e);
-
         }
     }
 }
