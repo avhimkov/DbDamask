40409b6e49fa6396ca8d47f9834432c88e8bd3bb
avhimkov <avhimkov@mail.ru>
2016-12-12 21:32:28 +0500

    add switch case

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,67 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
+        /*выбор типа сортировки*/
+        System.out.println("Тип БД");
+        BufferedReader inputType = new BufferedReader(new InputStreamReader(System.in));
+        String type = inputType.readLine();
 
-        try {
-            /*чтение файла конфигурации*/
-            InputStream myFile = new BufferedInputStream(new FileInputStream("config.txt"));
-            Scanner myScan = new Scanner(myFile, "windows-1251");
-            String line = myScan.nextLine();
+        switch (type) {
+            case "access": //for Access
+                try {
+                    /*чтение файла конфигурации*/
+                    InputStream myFile = new BufferedInputStream(new FileInputStream("config.txt"));
+                    Scanner myScan = new Scanner(myFile, "windows-1251");
+                    String line = myScan.nextLine();
 
-            String db = "jdbc:ucanaccess://"+line+"archive.mdb"; //D:\1\DamaskLN\
+                    String db = "jdbc:ucanaccess://" + line + "archive.mdb"; //D:\1\DamaskLN\
 
-            List<String> list = new ArrayList<>();
-            Files.lines(Paths.get("ticket.txt"), StandardCharsets.UTF_8).forEach(list::add);
-            for (String name : list){
-                String text = "'"+name+"'";
-                System.out.println(name);
+                    List<String> list = new ArrayList<>();
+                    Files.lines(Paths.get("ticket.txt"), StandardCharsets.UTF_8).forEach(list::add);
+                    for (String name : list) {
+                        String text = "'" + name + "'";
+                        System.out.println(name);
 
-                Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-                Connection conn = DriverManager.getConnection(db);
-                Statement st = conn.createStatement();
-                st.execute("DELETE FROM Process WHERE Ticket="+text);
-                st.execute("DELETE FROM Terminal WHERE Ticket="+text);
+                        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+                        Connection conn = DriverManager.getConnection(db);
+                        Statement st = conn.createStatement();
+                        st.execute("DELETE FROM Process WHERE Ticket=" + text);
+                        st.execute("DELETE FROM Terminal WHERE Ticket=" + text);
 
-                System.out.println("Delete");
-            }
-        } catch (Exception e) {
-            System.out.println(e);
+                        System.out.println("Delete");
+                    }
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+
+            case "mssql": //for MS SQL
+//                try {
+//                    /*чтение файла конфигурации*/
+//                    InputStream myFile = new BufferedInputStream(new FileInputStream("config.txt"));
+//                    Scanner myScan = new Scanner(myFile, "windows-1251");
+//                    String line = myScan.nextLine();
+//
+//                    String db = "jdbc:ucanaccess://" + line + "archive.mdb"; //D:\1\DamaskLN\
+//
+//                    List<String> list = new ArrayList<>();
+//                    Files.lines(Paths.get("ticket.txt"), StandardCharsets.UTF_8).forEach(list::add);
+//                    for (String name : list) {
+//                        String text = "'" + name + "'";
+//                        System.out.println(name);
+//
+//                        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+//                        Connection conn = DriverManager.getConnection(db);
+//                        Statement st = conn.createStatement();
+//                        st.execute("DELETE FROM Process WHERE Ticket=" + text);
+//                        st.execute("DELETE FROM Terminal WHERE Ticket=" + text);
+//
+//                        System.out.println("Delete");
+//                    }
+//                } catch (Exception e) {
+//                    System.out.println(e);
+//                }
         }
+
     }
 }
