fb2d377a4295d97f8dc453f115615fc864b02099
avhimkov <avhimkov@mail.ru>
2016-12-12 21:07:09 +0500

    add loop delete ticket & edit pom xml

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,8 +6,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
-
 
 public class Main {
 
@@ -22,22 +19,20 @@ public class Main {
 
             String db = "jdbc:ucanaccess://"+line+"archive.mdb"; //D:\1\DamaskLN\
 
-//            BufferedReader inputTicket = new BufferedReader(new InputStreamReader(System.in));
-//            String ticket = inputTicket.readLine();
             List<String> list = new ArrayList<>();
             Files.lines(Paths.get("ticket.txt"), StandardCharsets.UTF_8).forEach(list::add);
-            //подстановка кавычек
-            String text = "'"+list+"'";
-            System.out.println(list);
+            for (String name : list){
+                String text = "'"+name+"'";
+                System.out.println(name);
 
-            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-            Connection conn = DriverManager.getConnection(db);
-            Statement st = conn.createStatement();
-            st.execute("DELETE FROM Process WHERE Ticket="+text);
-            st.execute("DELETE FROM Terminal WHERE Ticket="+text);
+                Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+                Connection conn = DriverManager.getConnection(db);
+                Statement st = conn.createStatement();
+                st.execute("DELETE FROM Process WHERE Ticket="+text);
+                st.execute("DELETE FROM Terminal WHERE Ticket="+text);
 
-            System.out.println("Delete");
-
+                System.out.println("Delete");
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
