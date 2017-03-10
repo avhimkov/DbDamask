e1331387ab9ea4c52fd6cdeb915630853bab66fb
avhimkov <avhimkov@mail.ru>
2017-03-10 14:48:32 +0500

    refactoring and comment

--- src/Main.java
+++ src/Main.java
@@ -9,66 +9,73 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws SQLException, IOException {
-        /*выбор типа сортировки*/
+        String numberTicket, one, two, three;
+        File src, dest;
+
+        /*Дата для генерации резервной копии*/
         Date d = new Date();
         SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy-hh.mm");
         String format = format1.format(d);
 
+        /*выбор типа сортировки */
 //        System.out.println("Тип БД");
 //        BufferedReader inputType = new BufferedReader(new InputStreamReader(System.in));
 //        String type = inputType.readLine();
 
+        /*Чтение файла конфигурации*/
         InputStream myFile = new BufferedInputStream(new FileInputStream("config.txt"));
         Scanner myScan = new Scanner(myFile, "windows-1251");
         String line = myScan.nextLine();
 
-        File src = new File(line + "archive.mdb");
-        File dest = new File(line + "archive" + "-" + format +".mdb");
-
+        /*Резервное копирование archive.mdb*/
+        src = new File(line + "archive.mdb");
+        dest = new File(line + "archive" + "-" + format + ".mdb");
         CopyFile.Copy(src, dest);
 
 //        switch (type) {
 //            case "access": //for Access
-                try {
-                    /*чтение файла конфигурации*/
-                    String db = "jdbc:ucanaccess://" + line + "archive.mdb"; //D:\1\DamaskLN\
+        try {
+            /*Чтение базы*/
+            String db = "jdbc:ucanaccess://" + line + "archive.mdb"; //D:\1\DamaskLN\
 
-                    List<String> list = new ArrayList<>();
-                    Files.lines(Paths.get("ticket.txt"), StandardCharsets.UTF_8).forEach(list::add);
-                    for (String name : list) {
-                        //no tested
-                        String one = name.substring(name.length() - 1);
-                        String two = name.substring(name.length() - 2, name.length() - 1);
-                        String trey = name.substring(name.length() - 3, name.length() - 2);
+            /*Удаление 0 в номерах тикетов*/
+            List<String> list = new ArrayList<>();
+            Files.lines(Paths.get("ticket.txt"), StandardCharsets.UTF_8).forEach(list::add);
+            for (String name : list) {
+                one = name.substring(name.length() - 1);
+                two = name.substring(name.length() - 2, name.length() - 1);
+                three = name.substring(name.length() - 3, name.length() - 2);
 
-                        if (trey.contains("0")) {
-                            trey = "";
-                            if (two.contains("0")) {
-                                two = "";
-                                if (one.contains("0")){
-                                    one = "";
-                                }else {
-                                    one = name.substring(name.length() - 1);
-                                }
-                            } else {
-                               two = name.substring(name.length() - 2, name.length() - 1);
-                            }
+                if (three.contains("0")) {
+                    three = "";
+                    if (two.contains("0")) {
+                        two = "";
+                        if (one.contains("0")) {
+                            one = "";
                         } else {
-                            trey = name.substring(name.length() - 3, name.length() - 2);
+                            one = name.substring(name.length() - 1);
                         }
-
-                        String text = "'" + name + trey + two + one + "'";
-                        System.out.println("Delete - " + text);
-
-                        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-                        Connection conn = DriverManager.getConnection(db);
-                        Statement st = conn.createStatement();
-                        st.execute("DELETE FROM Process WHERE Ticket=" + text);
-                        st.execute("DELETE FROM Terminal WHERE Ticket=" + text);
+                    } else {
+                        two = name.substring(name.length() - 2, name.length() - 1);
                     }
-                } catch (Exception e) {
-                    System.out.println(e);
+                } else {
+                    three = name.substring(name.length() - 3, name.length() - 2);
                 }
+
+                /*Генерация номеров тикетов*/
+                numberTicket = "'" + name + three + two + one + "'";
+
+                /*Генерация SQL запроса к базе*/
+                Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+                Connection conn = DriverManager.getConnection(db);
+                Statement st = conn.createStatement();
+                st.execute("DELETE FROM Process WHERE Ticket=" + numberTicket);
+                st.execute("DELETE FROM Terminal WHERE Ticket=" + numberTicket);
+                System.out.println("Delete - " + numberTicket);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
 //            case "mssql": //for MS SQL
 
@@ -84,7 +91,8 @@ public class Main {
 //                Statement stmt = con.createStatement();
 //
 //                //query to be run against pkms
-//                ResultSet rs = stmt.executeQuery("SELECT * FROM ds_orders");
+//                ResultSet rs = stmt.executeQuery("DELETE FROM Process WHERE Ticket=" + numberTicket);
+//                ResultSet rs = stmt.executeQuery("DELETE FROM Terminal WHERE Ticket=" + numberTicket);
 //
 //                while (rs.next()) {
 //                    String str = rs.getString(1)+ "  " + rs.getString(2) + "  " + rs.getString(3);
